baa93ed3b9b1aa45af0ea536eaa99cfa5b46837e
Patrick Fournier <p_fournier@hushmail.com>
2025-09-15 23:58:35 -0400

    Met en ligne le devoir 1 (première partie)

---
+++ devoir2/reponses.R
@@ -0,0 +1,15 @@
+## Exercice 1
+## (a)
+set <- function(E, k) {
+    ## Votre code ici
+}
+
+## (b)
+setn <- function(E) {
+    ## Votre code ici
+}
+
+## (c)
+set_crible <- function(E) {
+    ## Votre code ici
+}
